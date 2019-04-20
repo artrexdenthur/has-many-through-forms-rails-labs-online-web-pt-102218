d407a1b52e41b40cea79f87209b4a8b46ccfbf58
Paul Ashour <paul.ashour@gmail.com>
2019-04-20 13:08:43 -0600

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
+  
+  def user_attributes=(user_attributes)
+    user = User.find_or_create_by(user_attributes)
+    # binding.pry
+    self.user = user if user.save
+  end
+
 
 end
